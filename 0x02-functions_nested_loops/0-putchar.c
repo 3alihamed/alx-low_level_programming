7faf104e5bad5c5e7ebea7746183087453ac645c
3alihamed <hamed.ali1996@gmail.com>
2023-07-26 18:10:30 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  * main - Entry point.
+ *   *
+ *    * Return: 0 indicate success.
+ *     */
+
+int main(void)
+{
+		char c[] = "_putchar";
+			int a;
+
+				for (a = 0 ; a < 8 ; a++)
+						_putchar (c[a]);
+					_putchar ('\n');
+						return (0);
+}
