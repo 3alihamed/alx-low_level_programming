ee7103c1ecfd9b820fe3319df7a792106a6e2287
3alihamed <hamed.ali1996@gmail.com>
2023-07-28 03:31:43 +0100

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,24 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <string.h>
-
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-
-
-
-
-
-
-
-
-
-
-
+int _putchar(char c);
+char _strcat(chardest, char src);
+char_strncat(char dest, charsrc, int n);
+char _strncpy(chardest, char src, int n);
+int _strcmp(chars1, char s2);
+void reverse_array(inta, int n);
+char string_toupper(chara);
+char cap_string(chara);
+char leet(chara);
+char rot13(chara);
+void print_number(int n);
+char infinite_add(charn1, char n2, charr, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
