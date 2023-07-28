0c7a1326bdbc69e998a8d6013db60944101ce90c
3alihamed <hamed.ali1996@gmail.com>
2023-07-28 02:20:02 +0100

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 char _strcat(chardest, char src);
-char_strncat(char dest, charsrc, int n);
+char _strncat(char dest, charsrc, int n);
 char _strncpy(chardest, char src, int n);
 int _strcmp(chars1, char s2);
 void reverse_array(inta, int n);
